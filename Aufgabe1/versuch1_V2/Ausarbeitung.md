d61a1daa5412e7e6df13e164a9ccd6d801be9590
Jan Holderied <holderja@hs-albsig.de>
2023-11-16 19:10:05 +0100

    till 3b

---
+++ Aufgabe1/versuch1_V2/Ausarbeitung.md
@@ -0,0 +1,19 @@
+# Ausarbeitung Versuch 1 ILS
+## Aufgabe 2
+### a
+Der Abstrakte Konstrukter der Klasse Classifier, übergibt den Paramter C an sein Attribut C. C legt die anzahl an möglichen Klassen fest, in die die Daten unterteilt werden können.
+
+Die Methode fit wird für das Trainieren des Klassifikators benutzt. Die Methode bekommt die Parameter X und T, wobei X die Daten und T die dazugehörigen Klassenlabel sind.
+
+Die Methode predict Klassifiziert anhand des Trainings neu übergabene Daten ein. Als Parameter übergibt man Predict z.B. einen neuen Vektor um ihn Klassifizieren zu lasse.
+
+Die Methode Crossvalidate teilt die Daten in S Teile auf. Der Parameter X ist für die Übergabe der Daten zuständig und der Parameter S gibt an, in wie viele Blöcke die Daten X geteilt werden sollen. Der dritte Parameter T sind die Klassenlabel für die Daten in X. Die Methode Crossvalidate macht eine Kreuzvalidierung mit dem Entsprachenden Modell in dem sie implementiert wird. Dies bedeutet, dass die Daten in X in S Blöcke geteilt werden und ein Block nicht zum Trainieren verwendet wird, sondern zum Validieren. Die Kreuzvalidierung macht jedoch mit jedem Block aus X eine Validierung und mittelt daraus dann die Genauigkeit.
+
+### b
+* Das Array in idxS enthält die Einträge bei den Parametern N=9 und S=3 [range(0, 3), range(3, 6), range(6, 9)]. Das Array aus 9 Daten soll in 3 gleich große Teile aufgeteilt werden. somit kommen dann die drei ranges zu stande.
+* idxVal enthält für jeden Schleifendurchgang einen der range Einträge aus der Liste idxS. idxTrain enthält die restlichen Blöcke die nicht in der Range von idxVal liegen.
+* In perm ist eine zufällige Reihenfolge aus Indexen für X. Dies ist sinnvoll, da wir die Daten in X nicht in der vorgegeben Reihenfolge verarbeiten wollen, da diese schon eine Art Ordung haben könnten. Somit indizieren wir mit einer Range aus den perm Indexen auf die Daten X und die Label T mit denen wir dann das Modell trainieren wollen.
+* 1 Durchlauf: x[3], x[1], x[0]
+2 Durchlauf: x[8], x[2], x[4]
+3 Durchlauf: x[7], x[5], x[6]
+* Der Returnwert err ist der prediction error, die Formel dafür befindet sich auf Seite 115 Nummer 2.108 (FP + FN) / (TP + FP + FN + TN). Der Returnwert MatCp ist die Confusion Matrix welche die Werte False Positive (FP), False Negative (FN), True Positive (TP) und True Negative (TN) enthält.
---
+++ Aufgabe1/versuch1_V2/V1A2/KNearestNeighborSearch.py
@@ -0,0 +1,52 @@
+import numpy as np
+
+def getKNearestNeighbors(x,X,k=1):  # realizes nearest neighbor search of x in database
+    """
+    compute the k nearest neighbors for a query vector x given a data matrix X
+    :param x: the query vector x
+    :param X: the N x D data matrix (in each row there is data vector) as a numpy array
+    :param k: number of nearest-neighbors to be returned
+    :return: return list of k line indixes referring to the k nearest neighbors of x in X
+    """
+    return np.argsort([np.linalg.norm(x - i) for i in X])[:k]           # REPLACE! return indexes of k smallest distances 
+
+def getClassProbabilities(t):
+    values, counts = np.unique(t, return_counts=True)
+    amountOfEachClass = dict(zip(values, counts))
+    return  dict(zip(values, [(amountOfEachClass[x] / len(t)) * 100  for x in amountOfEachClass])) #Prozentuale vorkommniswarscheinlichkeit der Klassen
+
+def classify(P):
+    return max(P, key=lambda k: (P[k], -list(P.keys()).index(k))) #sortiert das dict nach den values und holt sich dann den letzten key 
+
+# *****************************************************************************
+# ***** MAIN PROGRAM: Test nearest neighbor search and classification  ********
+# *****************************************************************************
+if __name__ == '__main__':
+    
+    # (i) Generate some dummy data 
+    X = np.array([[1,2,3],[-2,3,4],[3,-4,5],[4,5,-6],[-5,6,7],[6,-7,8]])   # data matrix X: list of data vectors (=database) of dimension D=3
+    t = np.array( [0     ,1       ,2       ,0       ,1       ,2      ] )   # class labels (C=3 classes)
+    C = np.max(t)+1                                                        # C=3 here
+    x = np.array([3.5,-4.4,5.3]);                                          # a new input vector to be classified
+    print("Data matrix X=\n",X)
+    print("Class labels t=",t)
+    print("Test vector x=",x)
+    
+    # (ii) Print all Euklidean distances to test vector x
+    print("Euklidean distances to x: ", [np.linalg.norm(X[i]-x) for i in range(len(X))])
+    
+    # (iii) Search for K nearest neighbor
+    K=3                                                    # define K
+    idx_KNN = getKNearestNeighbors(x,X,K)                  # get indexes of k nearest neighbors
+    print("idx_KNN=",idx_KNN)
+    print("The K Nearest Neighbors of x are the following vectors:")
+    for i in range(K):
+        idx=idx_KNN[i]
+        print("The", i+1, "th nearest neighbor is: X[",idx,"]=",X[idx]," with distance ", np.linalg.norm(X[idx]-x)," and class label ",t[idx])
+
+    # (iv) do classification
+    P=getClassProbabilities(t)                # get class probabilities for input x
+    c=classify(P)                                          # get most likely class
+        
+    print("Class distribution P=",P)
+    print("Most likely class: c=",c," with P(c)=",P[c])
